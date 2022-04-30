4536dd71b4f442778114ec25228d8a60b168d9a3
Gimmeursocks <bobteen1@gmail.com>
2022-04-30 22:50:51 +0200

    covid table and js intiation

--- script/covidscript.js
+++ script/covidscript.js
@@ -1,14 +1,16 @@
-var acc = document.getElementsByClassName("accordion");
-        var i;
-        
-        for (i = 0; i < acc.length; i++) {
-          acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var desc = this.nextElementSibling;
-            if (desc.style.maxHeight) {
-              desc.style.maxHeight = null;
-            } else {
-              desc.style.maxHeight = desc.scrollHeight + "px";
-            } 
-          });
-        }
+window.addEventListener("load",function(){
+  var acc = document.getElementsByClassName("accordion");
+          var i;
+          
+          for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function() {
+              this.classList.toggle("active");
+              var desc = this.nextElementSibling;
+              if (desc.style.maxHeight) {
+                desc.style.maxHeight = null;
+              } else {
+                desc.style.maxHeight = desc.scrollHeight + "px";
+              } 
+            });
+          }
+})
