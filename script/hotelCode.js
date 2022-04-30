9fb670266f1cd7ebc4b3dedd3fcbc728a5e34ad0
Gimmeursocks <bobteen1@gmail.com>
2022-04-30 22:52:02 +0200

    hotel js

--- script/hotelCode.js
+++ script/hotelCode.js
@@ -1,4 +1,8 @@
+window.addEventListener("load", function(){
+    
 const inerImage = document.getElementsByClassName("inerimg");
 const buttonr = document.getElementById("ribtn");
 const buttonl = document.getElementById("lebtn");
 const img = document.createElement(img);
+
+})
