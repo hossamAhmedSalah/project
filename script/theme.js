fa62f2531b0b6439be53508922c589fc913dc68d
Gimmeursocks <bobteen1@gmail.com>
2022-05-01 09:30:31 +0200

    tryna fix code

--- script/theme.js
+++ script/theme.js
@@ -55,7 +55,7 @@ function dark(){
 
 function getCookie(str){
     if(document.cookie == ''){
-        return "";
+        return '';
     }
     const cookieValue = document.cookie.split('; ').find(row => row.startsWith(str)).split('=')[1];
     return cookieValue;
